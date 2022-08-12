7dcbd19ec8176e64dce9cdb049c298540f4f04ed
Joaomgsa <joaomgsa@gmail.com>
2022-08-12 11:41:56 -0300

    adicionando o projeto inicial

---
+++ src/main/java/br/com/elatrampa/ElatrampaApplication.java
@@ -0,0 +1,13 @@
+package br.com.elatrampa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElatrampaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElatrampaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/elatrampa/ElatrampaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elatrampa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElatrampaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
