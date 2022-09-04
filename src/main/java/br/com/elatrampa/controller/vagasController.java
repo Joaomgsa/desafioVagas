0cf5a81f6ade175d82d6f5e53bd9aa34ef549543
Joaomgsa <joaomgsa@gmail.com>
2022-09-04 11:45:10 -0300

    refatorando o controller vagas

--- src/main/java/br/com/elatrampa/controller/vagasController.java
+++ src/main/java/br/com/elatrampa/controller/vagasController.java
@@ -2,11 +2,17 @@ package br.com.elatrampa.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.elatrampa.repository.VagasRepository;
+import br.com.elatrampa.model.VagasModel;
 
 @RestController
 @RequestMapping({"/vagas"})
@@ -23,9 +29,18 @@ public class VagasController {
 		return repository.findAll();
 	}
 	
-	@GetMapping(path = {"/{vagaId}"})
-	public ResponseEntity<Vagas> findByid(@PathVariable long vagaId,
-			@RequestBody Vagas vagas) {
-		return  repository.findById(vagas.get)
+	@PostMapping
+	public VagasModel create(@RequestBody VagasModel vagas) {
+		return repository.save(vagas);
 	}
+	
+	@DeleteMapping(path = {"/{vagaId}"})
+	public ResponseEntity<?> delete(@PathVariable("vagaId") long vagaId) {
+		return repository.findById(vagaId)
+				.map(record ->{
+					repository.deleteById(vagaId);
+					return ResponseEntity.ok().build();
+				}) .orElse(ResponseEntity.notFound().build());
+	}
+	
 }
--- src/main/java/br/com/elatrampa/model/VagasModel.java
+++ src/main/java/br/com/elatrampa/model/VagasModel.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity(name="Vaga")
+@Entity(name="Vagas")
 
 public class VagasModel {
 	
