d8c6cb2a3a8a448e7f3bf265f73611479d911f36
Joaomgsa <joaomgsa@gmail.com>
2022-09-06 09:26:31 -0300

    escrevendo scripts sql

---
+++ consultasSql/experiencia.sql
@@ -0,0 +1,5 @@
+INSERT into experiencia (descricao, experiencia_valor)
+VALUES ('junior', 2),
+	   ('pleno', 3),
+	   ('senior', 4),
+	   ('especialista', 5);
---
+++ consultasSql/localidade.sql
@@ -0,0 +1,8 @@
+INSERT into localidade  (localidade , distancia)
+VALUES ('A', 1,
+	   ('B', 5),
+	   ('C', 9),
+	   ('D', 3),
+	   ('E', 10),
+	   ('F', 8);
+	   
