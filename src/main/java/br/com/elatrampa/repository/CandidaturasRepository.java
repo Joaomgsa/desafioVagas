ec6df3222cc46341c698c2ca1606af4a17351925
Joaomgsa <joaomgsa@gmail.com>
2022-09-12 09:45:05 -0300

    escrevendo query ranking candidaturas

--- src/main/java/br/com/elatrampa/repository/CandidaturasRepository.java
+++ src/main/java/br/com/elatrampa/repository/CandidaturasRepository.java
@@ -1,9 +1,17 @@
 package br.com.elatrampa.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.elatrampa.model.CandidaturasModel;
 
 public interface CandidaturasRepository extends JpaRepository <CandidaturasModel, Long>{
 
+	
+	@Query("select c from candidaturas c where c.candidatura = :candidatura")
+	public List<CandidaturasModel> rankingCandidaturas(
+			@Param("candidatura") CandidaturasModel candidaturaId);
 }
