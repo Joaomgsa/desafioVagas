7dbc1bb8ce27464d8d681e686d0d4c18d5e35c51
Joaomgsa <joaomgsa@gmail.com>
2022-09-26 13:55:18 -0300

    reescrevendo os servicos

--- src/main/java/br/com/elatrampa/converter/DozerConverter.java
+++ src/main/java/br/com/elatrampa/converter/DozerConverter.java
@@ -1,5 +1,30 @@
 package br.com.elatrampa.converter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+
+import br.com.elatrampa.model.PessoasModel;
+import br.com.elatrampa.vo.PessoaVo;
+
+
 public class DozerConverter {
 
+	private static Mapper mapper = new DozerBeanMapper();
+	
+	public static <O, D> D parseObject(O origin, Class<D> destination) {
+		return mapper.map(origin, destination);
+	}
+	
+	public static <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination) {
+		List<D> destinationObjects = new ArrayList<D>();
+		for (Object o : origin) {
+			destinationObjects.add(mapper.map(o, destination));
+		}
+		return destinationObjects;
+	}
+
+	
 }
