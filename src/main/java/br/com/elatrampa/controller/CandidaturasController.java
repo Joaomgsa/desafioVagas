07a4afcddfad3645c48cf3399b22b479626cdf90
Joaomgsa <joaomgsa@gmail.com>
2022-09-27 19:28:53 -0300

    adicionando novo metodo no controller candidaturas

--- src/main/java/br/com/elatrampa/controller/CandidaturasController.java
+++ src/main/java/br/com/elatrampa/controller/CandidaturasController.java
@@ -16,16 +16,18 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.elatrampa.repository.CandidaturasRepository;
+
 import br.com.elatrampa.services.CandidaturasService;
 import br.com.elatrampa.vo.CandidaturasVo;
-import br.com.erudio.controller.BookController;
-import br.com.erudio.data.vo.v1.BookVO;
+
+
 
 @Tag(name = "Candidaturas Endpoint")
 @RestController
@@ -64,4 +66,14 @@ public class CandidaturasController {
 		candidaturasVo.add(linkTo(methodOn(CandidaturasController.class).findById(id)).withSelfRel());
 		return candidaturasVo;
 	}	
+	
+	@Operation(summary = "Criar um novo livro")
+	@PostMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, 
+			consumes = { "application/json", "application/xml", "application/x-yaml" })
+	public CandidaturasVo create(@RequestBody CandidaturasVo candidatura) {
+		CandidaturasVo candidaturasVo = service.create(candidatura);
+		candidaturasVo.add(linkTo(methodOn(CandidaturasController.class).findById(candidaturasVo.getCandidaturaKey())).withSelfRel());
+		return candidaturasVo;
+	}
+	
 }
