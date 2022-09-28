7012f31dd1feb90917bb092e355b792cffa0e736
Joaomgsa <joaomgsa@gmail.com>
2022-09-28 12:18:23 -0300

    removendo algumas dependencias que não foram utilizadas

--- src/main/java/br/com/elatrampa/ElatrampaApplication.java
+++ src/main/java/br/com/elatrampa/ElatrampaApplication.java
@@ -1,11 +1,16 @@
 package br.com.elatrampa;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class ElatrampaApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ElatrampaApplication.class, args);
 	}
---
+++ src/main/java/br/com/elatrampa/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package br.com.elatrampa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+
+@Configuration
+public class OpenApiConfig {
+
+	@Bean
+	public OpenAPI customOpenApi() {
+		return new OpenAPI()
+		        .info(new Info()
+		                .title("RESTful API Elatrampa")
+		                .version("v1")
+		                .description("Some description about your API."));
+	}
+}
