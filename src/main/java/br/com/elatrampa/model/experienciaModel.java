ab2fde67480eae83ea77c1cbfecf6b1e37724e27
Joaomgsa <joaomgsa@gmail.com>
2022-09-28 12:19:06 -0300

    limpando codigo

--- src/main/java/br/com/elatrampa/model/experienciaModel.java
+++ src/main/java/br/com/elatrampa/model/experienciaModel.java
@@ -10,9 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 
 @Entity
